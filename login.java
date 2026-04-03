8db0525ba03c6b938a662c529ddbf28d0200e025
AnitaS2007 <anitahylakar@gmail.com>
2026-04-03 11:37:59 +0530

    Java login program

---
+++ login.java
@@ -0,0 +1,12 @@
+public class login{
+  public static void main(string[] args){
+    string username="admin";
+    string password="1234";
+    if(username.equals("admin") && password.equals("1234")){
+      system.out.println("login successfull");
+    }
+    else{
+      system.out.println("login failed");
+    }
+  }
+}
